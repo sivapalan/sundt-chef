92e66b18a80c58367d931a18cdd952b330cf7f87
sivapalan <5470233+sivapalan@users.noreply.github.com>
2018-01-23 15:06:23 +0100

    Add API module

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const api = require('./lib/api');
 const dialogflow = require('./lib/dialogflow');
 
+exports.getMenuForDay = api.getMenuForDay;
+exports.getMenusForWeek = api.getMenusForWeek;
 exports.dialogflowChefFulfillment = dialogflow.dialogflowChefFulfillment;
