e8e3f160d0b72a7589df7cae357a1ce8ade31aa0
sivapalan <5470233+sivapalan@users.noreply.github.com>
2018-01-23 15:06:23 +0100

    Organize files in lib directory

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ if (dotenv.error) {
     throw dotenv.error;
 }
 
-const api = require('./lib/api');
-const dialogflow = require('./lib/dialogflow');
+const api = require('./lib/interface/api');
+const dialogflow = require('./lib/interface/dialogflow');
 
 exports.getMenuForDay = api.getMenuForDay;
 exports.getMenusForWeek = api.getMenusForWeek;
